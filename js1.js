b44a067688abe288e7149f9030150918753a0ceb
Paul <paul.hoha@yahoo.com>
2021-05-31 22:42:26 +0300

    dsd

---
+++ js1.js
@@ -0,0 +1,6 @@
+//Ecuatia de grad 1 cu numerele 5 si 5 :
+var a=5;
+var x;
+var b=5;
+document.write("Ecuatia este: "+a+"x"+"+"+b+"="+"0"+"<br><br>"); 
+document.write("Solutia ecuatiei este:"+"<br>"+-b/a);
